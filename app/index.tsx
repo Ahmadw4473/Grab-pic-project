900b646066ee92eb7503dc0fda77d342a9125ac6
Ahmadw4473 <ahmadw4473@gmail.com>
2026-06-25 17:12:06 +0500

    added image uploader using expo-image-picker

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,60 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View, Button, Pressable, Alert, Image } from 'react-native'
+import React, { useState } from 'react'
+import * as ImagePicker from 'expo-image-picker'
+import { Assets } from '@react-navigation/elements';
 
+const index = () => {
 
-export default function Index() {
+  const [image, setImage] = useState<any>([]);
+
+  async function pickImage() {
+    const permission = await ImagePicker.requestMediaLibraryPermissionsAsync()
+    if (permission.granted == false) {
+      Alert.alert('Permission denied')
+      return
+    }
+
+    const image = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ['images', 'videos'],
+      // allowsEditing: true,
+      // aspect:,
+      allowsMultipleSelection: true,
+      quality: 1,
+    })
+
+    if (!image.canceled && image.assets && image.assets.length > 0) {
+      let uris = image.assets.map((assets) => {
+        return assets
+      })
+      setImage((prev: any) => {
+        return [...prev, ...uris]
+      });
+    }
+
+  }
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View>
+      <Button title='upload' onPress={pickImage}></Button>
+      {
+
+        image.map((images: any, index: number) => (
+          <View key={index}>
+            <Image style={styles.image} source={{ uri: images.uri }} />
+          </View>
+        ))
+      }
+
     </View>
-  );
+
+  )
 }
+
+export default index
+
+const styles = StyleSheet.create({
+  image: {
+    height: 100,
+    width: 100
+  }
+})
